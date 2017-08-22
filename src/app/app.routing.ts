e62027e46f5300f18ae70cbd7499bb67a485bd10
Calla Rudolph and Nathan Stewart <nathan@>
2017-08-22 09:06:10 -0700

    add info component, add routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { InfoComponent  } from './info/info.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: InfoComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
